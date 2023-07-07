6a57ac19bb3e800b42b9293a88271a66626c9ff6
joshlin5 <linjos6750@hgmail.com>
2023-07-06 20:01:01 -0400

    Updated siteUrl in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://main.dw0f2i4kppeqc.amplifyapp.com/`
   },
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-mdx", {
     resolve: 'gatsby-source-filesystem',
